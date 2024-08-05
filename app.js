e3d22a3b9653b1e2f833a4ee23eaf94478ba252c
shoaib inamdar <shoaibinamdar14@gmail.com>
2024-08-05 10:47:39 +0530

    skeleton loader added && search on pressing enter

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ var meaningtag=document.querySelector('.meaningtag')
 var wordtag=document.querySelector('.wordtag')
 var arr=["10-code","2,3-diphosphoglycerate","Abandonment","ABCs"]
 var btn=document.querySelector('#searchicon')
+var loading=document.querySelector(".skeletonload");
+
 
 async function getdata(searchedword){
+    wordtag.innerHTML = ''
+    meaningtag.innerHTML =''
     let response=await fetch(url)
     var data=await response.json();
     // console.log(data)
@@ -13,17 +17,33 @@ async function getdata(searchedword){
         return e.word.toLowerCase() === searchedword.toLowerCase();
         // console.log(e)
     })
-    wordtag.innerHTML = result[0].word
-    meaningtag.innerHTML = result[0].meaning
+    loading.style.opacity=1;
+    setTimeout(()=>{
+        loading.style.opacity=0;
+        // console.log("hello")
+    },2000)
+    setTimeout(() => {
+        wordtag.innerHTML = result[0].word
+        meaningtag.innerHTML = result[0].meaning
+    }, 2000);
     console.log(result[0].meaning)
     // meaningtag.innerHTML=n.meaning;
     // wordtag.innerHTML=data[5].word;
-    
+    document.querySelector("#search").value='';
 }
 
-btn.addEventListener("click",(e)=>{
+function search(){
     var searchedword=document.querySelector('#search').value;
     getdata(searchedword);
+}
+btn.addEventListener("click",(e)=>{
+    search();
 })
 // console.log(promise);
 // console.log(word)
+
+document.onkeydown=function(e){
+    switch(e.keyCode){
+        case 13:search();
+    }
+}
